e5f4611ede8b0833f9dd667bc75b52d570863e5c
rmcclorey1125 <rmcclorey1@yahoo.com>
2021-01-23 09:41:17 -0500

    pics done

--- pic/src/api/unsplash.js
+++ pic/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID 48HwuKymr9kJGt6ikw-QYAaLkW7pXGxQj5PBfU_Nby0'
+        Authorization: 'Client-ID'
     }
 });
